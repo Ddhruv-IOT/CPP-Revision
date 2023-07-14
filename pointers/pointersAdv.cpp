333dac15ba680f02e40d048507d30825c5c7718c
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-07-14 19:16:56 +0530

    array and symbol table error

--- pointers/pointersAdv.cpp
+++ pointers/pointersAdv.cpp
@@ -31,4 +31,9 @@ int main()
     std::cout << "The address of first element: " << p << "\n";     // address of first element
     std::cout << "The value of first element: " << *p << "\n";      // value of first element
     std::cout << "The address of pointer variable: " << &p << "\n"; // address of pointer variable
+
+
+    int arr3[] = {10, 20, 30, 40, 50};
+    // &arr3 = &arr3 + 1; // This is not allowed
+    // The above will give an error as the address of an array is constant and cannot be changed. The address is mapped in Symbol Table.
 }
