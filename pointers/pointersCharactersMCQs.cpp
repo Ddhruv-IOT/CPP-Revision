90279092ee0cf20020f11f29ccdcaea97e7f1bfa
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-07-17 23:25:44 +0530

    new question addedp

--- pointers/pointersCharactersMCQs.cpp
+++ pointers/pointersCharactersMCQs.cpp
@@ -1,14 +1,16 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     char s[] = "hello";
     char *p = s;
     cout << s[0] << " " << p[0] << "\n\n";
 
     char arr[20];
     int i;
-    for(i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++)
+    {
         *(arr + i) = 65 + i;
     }
     *(arr + i) = '\0';
@@ -19,4 +21,29 @@ int main() {
     ptr = Str;
     ptr += 5;
     cout << ptr << "\n\n";
+
+    int numbers[5];
+    int *p1;
+    p1 = numbers;
+    *p1 = 10;
+    p1 = &numbers[2];
+    *p1 = 20;
+    p1--;
+    *p1 = 30;
+    p1 = numbers + 3;
+    *p1 = 40;
+    p1 = numbers;
+    *(p1 + 4) = 50;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << numbers[i] << " ";
+    }
+    cout << "\n\n";
+
+    char st1[] = "ABCD";
+
+    for (int i = 0; st1[i] != '\0'; i++)
+    {
+        cout << st1[i] << *(st1)+i << *(i + st1) << i[st1] << "\n";
+    }
 }
