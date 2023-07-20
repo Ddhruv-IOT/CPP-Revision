d91b6a3e8d264125c77ed57308e5208028d35a13
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-07-20 23:44:01 +0530

    functions also done

--- pointers/pointersFunctionsMCQs.cpp
+++ pointers/pointersFunctionsMCQs.cpp
@@ -24,6 +24,19 @@ void swap(char *x, char *y)
     y = t;
 }
 
+// Function for Question 4
+void Q(int z) {
+    z += z;
+    cout << z << " ";
+}
+
+void P(int *y) {
+    int x = *y + 2; // 7
+    Q(x);
+    *y = x - 1; // 6
+    cout << x << " "; // 7 
+}
+
 int main()
 {
 
@@ -42,9 +55,23 @@ int main()
     char *y = "codingninjas";
     char *t;
     swap(x, y);
-    cout << x << " " << y <<" ";
+    cout << x << " " << y << " ";
     t = x;
     x = y;
     y = t;
     cout << x << " " << y << "\n\n";
+
+    // Question 4
+    int x1 = 5;
+    P(&x1); 
+    cout << x1 << "\n\n";
+
+    int a9 = 10;
+    int *p = &a9;
+    int **q = &p;
+    int b9 = 20;
+    *q = &b9;
+    (*p)++;
+    cout << a9 << " " << b9 << "\n\n";
+
 }
