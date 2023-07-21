1c564b05a222eb41d39a8ffa0cbed1d803076018
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-07-21 07:03:15 +0530

    space in ptr declartion dosen't matters

--- pointers/doublePointers.cpp
+++ pointers/doublePointers.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-    
+
+    int a = 9;
+    int* p = &a; // * just after int is also valid
+    int *q = &a; // * just before q is also valid
+    int * z = &a; // This is also valid
+    int*k = &a; // This is also valid
+    cout << p << " " << q << " " << z << " " << k << "\n\n";
+
+
 }
