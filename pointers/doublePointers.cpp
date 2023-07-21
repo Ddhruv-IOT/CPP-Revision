25815f235f0cc41c5a2d3ce78e24d0ddddb1e76b
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-07-21 07:38:50 +0530

    question 4 Seg

--- pointers/doublePointers.cpp
+++ pointers/doublePointers.cpp
@@ -20,6 +20,31 @@ int main() {
     cout << **ptr_ptr << " " << ptr_ptr << " " << &ptr_ptr << "\n";
     cout << &ptr_ptr << "->" << &ptr << "->" << &i << "\n\n";
 
+    // question 1
+    int first = 8;
+    int second = 10;
+    int *ptr3 = &second;
+    *ptr3 = 9;
+    cout << first << " " << second << "\n\n";
+
+    // question 2
+    int first1 = 6;
+    int *p2 = &first1;
+    int *q2 = p2;
+    (*q2)++;
+    cout << first1 << " " << *p2 << " " << *q2 << "\n\n";
+
+    // question 3
+    int first2 = 8;
+    int *p3 = &first2;
+    cout << (*p3)++ << " " ;
+    cout << first2 << " " << "\n\n";
+
+    // question 4
+    int *p5 = 0;
+    int first5 = 110;
+    // *p5 = first5; // This will give a segmentation fault
+    // cout << *p5 << "\n\n"; // gives segmentation fault
 
 
 
