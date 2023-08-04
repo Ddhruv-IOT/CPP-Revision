567f8cd57d2b3402a56e638919f6e8592a6558d7
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-08-05 00:29:46 +0530

    return by reference and inline function call

--- memory/mem1.cpp
+++ memory/mem1.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 // A function to increment by 1 through pass by refrence
-
 void update(int &a) {
     a++;
+}
+
+// A function to increment by 1 through pass by value with return type as a reference variable
+// here space between int & and function name is not necessary
+// int& function name is same as int &function name and even int&function name, also int & function name
+int& update2(int &a) {
+    a++;
+    return a;
 }
 
 int main() {
@@ -26,4 +33,8 @@ int main() {
     cout << "Value of c Before update: " << c << endl;
     update(c);
     cout << "Value of c After update: " << c << endl;
+
+    int d = 2;
+    cout << "Value of d Before update: " << d << endl;
+    cout << "Value of d After update: " << update2(d) << endl;
 }
