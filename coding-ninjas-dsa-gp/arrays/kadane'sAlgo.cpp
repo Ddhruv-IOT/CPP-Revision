f29a66d37b2d6e643c0f72a6d57a6dbc744b8de7
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-08-06 09:59:00 +0530

    foramtted it

--- coding-ninjas-dsa-gp/arrays/kadane'sAlgo.cpp
+++ coding-ninjas-dsa-gp/arrays/kadane'sAlgo.cpp
@@ -5,22 +5,21 @@
 #include <math.h>
 using namespace std;
 
-int main(){
-    int maxi = INT_MIN;
+int main()
+{
+    int maxSum = INT_MIN;
     int currSum = 0;
-    int arr[] = {-1, 2, -2, 5, 7,-2, 10, 50, -3, 1};
+    int arr[] = {-1, 2, -2, 5, 7, -2, 10, 50, -3, 1};
 
-    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
         currSum += arr[i];
-        // if (currSum > 0 && currSum > maxi) {
-        //     maxi = currSum;
-        // }
-        maxi = max(maxi, currSum);
-        // currSum = maxi;
-        
-        if (currSum < 0) {
+        maxSum = max(maxSum, currSum);
+
+        if (currSum < 0)
+        {
             currSum = 0;
         }
     }
-    cout << maxi << endl;
+    cout << "The maximum subarray sum is: " << maxSum << endl;
 }
